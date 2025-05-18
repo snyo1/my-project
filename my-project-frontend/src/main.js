fb02e41aa3a88752db6cd3b99c2a616de1787073
Snyo <3472533060@qq.com>
2025-05-18 11:55:55 +0800

    完成跨域手动配置，登录弹窗有bug

--- my-project-frontend/src/main.js
+++ my-project-frontend/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from "@/router";
 import axios from "axios";
 
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
 axios.defaults.baseURL = 'http://localhost:8080'
 
 const app = createApp(App)
